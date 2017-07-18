477aebc04ad19044b485238d2a87a45305ccec0f
khoeger <katarina.hoeger@gmail.com>
2017-07-18 14:17:00 -0400

    Pass variable to html file

--- python/packages/flask/bevatron_tutorial/application_lulu.py
+++ python/packages/flask/bevatron_tutorial/application_lulu.py
@@ -4,7 +4,8 @@ app_lulu = Flask(__name__)
 
 @app_lulu.route('/index_lulu')
 def index_lulu():
-    return(render_template('userinfo_lulu.html'))
+    nquestions= 5
+    return(render_template('userinfo_lulu.html',num=nquestions))
 
 if __name__ == '__main__':
     app_lulu.run(debug=True)
