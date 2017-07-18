039f251cedb6ab0557c2f8a005ff8d65a5d56bc2
khoeger <katarina.hoeger@gmail.com>
2017-07-18 14:35:12 -0400

    Has methods GET and POST

--- python/packages/flask/bevatron_tutorial/application_lulu.py
+++ python/packages/flask/bevatron_tutorial/application_lulu.py
@@ -2,13 +2,13 @@
 from flask import Flask , render_template , request
 app_lulu = Flask(__name__)
 
-@app_lulu.route('/index_lulu',methods=['GET'])
+@app_lulu.route('/index_lulu',methods=['GET','POST'])
 def index_lulu():
     nquestions= 5
     if request.method == 'GET':
         return(render_template('userinfo_lulu.html',num=nquestions))
     else:
-        return 'request.method was not a GET!'
+        return('request.method was not a GET!')
 
 if __name__ == '__main__':
     app_lulu.run(debug=True)
