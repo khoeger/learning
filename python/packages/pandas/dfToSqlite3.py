4ec8d84c712e4f61fd49ee7b899461b0b83517d5
khoeger <katarina.hoeger@gmail.com>
2017-08-10 15:12:15 -0400

    Commit pandas learning files

--- python/packages/pandas/dfToSqlite3.py
+++ python/packages/pandas/dfToSqlite3.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import sqlite3
 
 pf = pd.DataFrame({ 'AAA' : [4,5,6,7],
                     'BBB' : [10,20,30,40],
                     'CCC' : [100,50,-30,-50]})
 
 print (pf)
+name1 = 'example.db'
+name2 = "NewSqlTable"
+
+conn = sqlite3.connect(name1)
+
+sqlDB = pf.to_sql(  name=name2,
+                    con=conn,
+                    #flavor='sqlite',
+                    schema=None,
+                    index_label = "Triple Letters")
+
+print(sqlDB)
+
+conn.clse()
